1e0aa8de7b4068de9090a2458d88dcc16d90c111
Fred <F.Schwabe@stud.uni-heidelberg.de>
2017-03-22 15:33:14 +0100

    Additional unit test for the knapsack solve method

--- main/knapsack_test.py
+++ main/knapsack_test.py
@@ -33,3 +33,55 @@ class TestKnapsackSolver(unittest.TestCase):
 		self.knapsack.addItem(0, 0)
 		self.assertEqual(self.knapsack.getItemCount(), 5)
 		self.knapsack.reset()
+
+	def test_solve_one(self):
+		"""
+		Test a simple example of the knapsack problem
+		"""
+		self.knapsack.addItem(3, 7)
+		self.knapsack.addItem(4, 5)
+		self.knapsack.addItem(7, 9)
+		self.knapsack.addItem(2, 1)
+		self.knapsack.addItem(1, 4)
+		value, itemList = self.knapsack.solve()
+		self.assertEqual(value, 17)
+		self.assertListEqual([0,2,3,4], itemList)
+		self.knapsack.reset()
+
+	def test_solve_two(self):
+		"""
+		Test of a sample, where every item is too big
+		"""
+		self.knapsack.addItem(11, 4)
+		self.knapsack.addItem(25, 30)
+		self.knapsack.addItem(14, 5)
+		value, itemList = self.knapsack.solve()
+		self.assertEqual(value, 0)
+		self.assertListEqual([], itemList)
+		self.knapsack.reset()
+
+	def test_solve_three(self):
+		"""
+		Test of a sample, with items without weight
+		"""
+		self.knapsack.addItem(0, 5)
+		self.knapsack.addItem(0, 4)
+		self.knapsack.addItem(0, 7)
+		self.knapsack.addItem(0, 2)
+		value, itemList = self.knapsack.solve()
+		self.assertEqual(value, 18)
+		self.assertListEqual([0,1,2,3], itemList)
+		self.knapsack.reset()
+
+	def test_solve_four(self):
+		"""
+		Test of a sample, where no item has a value
+		"""
+		self.knapsack.addItem(4, 0)
+		self.knapsack.addItem(3, 0)
+		self.knapsack.addItem(5, 0)
+		self.knapsack.addItem(3, 0)
+		value, itemList = self.knapsack.solve()
+		self.assertEqual(value, 0)
+		self.assertListEqual([0,1], itemList)
+		self.knapsack.reset()
